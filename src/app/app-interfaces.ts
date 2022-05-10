6c2ff2c49aa667f215615a2902d44840ae5e4891
Shachar Ovadya <ovadya.shachar@gmail.com>
2022-05-10 07:37:26 +0300

    Adjust IEnvironment, IContact interfaces

--- src/app/app-interfaces.ts
+++ src/app/app-interfaces.ts
@@ -1,11 +1,21 @@
 export interface IEnvironment {
     production: boolean;
     contactsApiBaseUrl: string;
+    firebaseConfig: IFirebaseConfig;
+}
+export interface IFirebaseConfig {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
 }
 
 export interface IContact {
     id: string | number;
     firstName: string;
     lastName: string;
-    birthDate: Date;
+    birthDate?: Date;
+    created?: Date
 }
