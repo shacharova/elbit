c082b548ae3162aedf257ef6c2c79b7d00d8e548
Shachar Ovadya <ovadya.shachar@gmail.com>
2022-05-18 08:02:54 +0300

    path for new contact

--- src/app/pages/contact-page/contact-page-routing.module.ts
+++ src/app/pages/contact-page/contact-page-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact-page.component';
 
 const routes: Routes = [
+  { path: '', component: ContactComponent },
   { path: ':id', component: ContactComponent }
 ];
 
