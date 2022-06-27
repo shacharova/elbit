a10515a7b084718fd58338b6a94829a2991170e1
Shachar Ovadya <shacharova@users.noreply.github.com>
2022-06-27 17:22:53 +0300

    sp-state

---
+++ src/app/sp-state.ts
@@ -0,0 +1,36 @@
+import { BehaviorSubject, distinctUntilChanged, map, Observable } from "rxjs";
+
+export class SpState<TState = any> {
+    private initialState: TState;
+    private state: BehaviorSubject<TState>;
+
+    constructor(initialState: TState) {
+        this.initialState = initialState;
+        this.state = new BehaviorSubject(initialState);
+    }
+
+    public getValue(): TState {
+        return this.state.getValue();
+    }
+
+    public select(): Observable<TState>;
+    public select<K extends keyof TState>(key: K): Observable<TState[K]>;
+    public select<R>(arg: (store: TState) => R): Observable<R>;
+    public select<R>(arg?: keyof TState | ((store: TState) => R)) {
+        const stateObs = this.state.asObservable().pipe(distinctUntilChanged());
+        if (typeof (arg) === 'function') {
+            return stateObs.pipe(map(arg));
+        } else if (typeof (arg) === 'string') {
+            return stateObs.pipe(map(s => s[arg]));
+        }
+        return stateObs;
+    }
+
+    public update(state: Partial<TState>) {
+        this.state.next({ ...this.getValue(), ...state });
+    }
+
+    public reset() {
+        this.state.next(Object.assign({}, this.initialState));
+    }
+}
